f85b51ff1ee6a97678735e73e424d8db35722576
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-04-24 17:46:17 -0700

    Rename game manager

--- Scenes/transition_scene.gd
+++ Scenes/transition_scene.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	game_manager._pick_next_game()
-	game_manager._change_to_next_game()
+	GameManager._pick_next_game()
+	GameManager._change_to_next_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
