0246e3305dde917525d5db374df2d0686aca29fd
Cal Friedman School <cffriedm@ucsc.edu>
2024-04-28 16:08:27 -0700

    More work on player input

--- Scripts/rumble_minigame.gd
+++ Scripts/rumble_minigame.gd
@@ -1,5 +1,9 @@
 class_name RumbleMinigame extends IMinigame
 
+@onready var root = get_tree().current_scene
+@onready var player_object = load("res://Resources/rumble_player.tscn")
+@onready var tower_object = load("res://Resources/transmission_tower.tscn")
+
 var players_count : int
 var players := {
 	
@@ -11,20 +15,25 @@ func _ready():
 	await start() #the following I think should only start after the player presses a button to close the controls screen Edit: it doesn't wait, apparently - not sure why
 	print("test")
 	for i in (players_count):
-		players[i] = (load("res://Resources/rumble_player.tscn")).instantiate() as RumblePlayer
+		players[i] = player_object.instantiate() as RumblePlayer
+		root.add_child(players[i])
 		print("instantiated player")
-		print(i)
+		#print(i)
 		players[i].player_id = i
+		print(players[i].player_id)
 	for i in (players_count):
 		players[i].position = Vector2(150, 150)
+		print("players[i].position:")
+		print(players[i].position)
 	
 	var t
 	for i in 5:
 		for j in 5:
-			t = load("res://Resources/transmission_tower.tscn").instantiate()
+			t = tower_object.instantiate() as TransmissionTower
+			root.add_child(t)
 			(t.position).x = i*64
 			(t.position).y = j*64
-			print(t.position)
+			#print(t.position)
 			
 	
 
--- Scripts/rumble_player.gd
+++ Scripts/rumble_player.gd
@@ -1,11 +1,11 @@
 class_name RumblePlayer extends Area2D
 
-var inputs = {
-	"move_right": Vector2.RIGHT,
-	"move_left": Vector2.LEFT,
-	"move_up": Vector2.UP,
-	"move_down": Vector2.DOWN
-}
+#var inputs = {
+#	"move_right": Vector2.RIGHT,
+#	"move_left": Vector2.LEFT,
+#	"move_up": Vector2.UP,
+#	"move_down": Vector2.DOWN
+#}
 
 @export var player_id := -1 as int
 @export var entered_tower_time := 0 as float
@@ -22,15 +22,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(player_id)
+	#print(player_id)
 	var direction = MultiplayerInput.get_vector(PlayerManager.get_player_device(player_id), "move_right", "move_left", "move_up", "move_down")
+	print(direction)
 	if direction != Vector2(0, 0):
 		print(player_id)
+		print("holding direction")
 		if (direction.x).abs() >= (direction.y).abs():
-			x_axis = 1
+			x_axis = 1 * (direction.x/((direction.x).abs())) #this expression gets the sign of the direction
 			y_axis = 0
 		else:
-			y_axis = 1
+			y_axis = 1 * (direction.y/((direction.y).abs())) #this expression gets the sign of the direction
 			x_axis = 0
 		
 		held_count += 1000*delta #add the number of milliseconds since the last frame
